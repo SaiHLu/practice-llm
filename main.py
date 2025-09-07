c98db602cae5f7306c54c29ada486b8aad9256c2
SaiHLu <saisailuhlaing@gmail.com>
2025-09-07 10:18:26 +0700

    message placeholder

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import (
+    HumanMessage,
+    SystemMessage,
+)
 
 load_dotenv()
 
@@ -30,7 +34,7 @@ If you don't know, just say 'I don\'t know' and don't try to make an answer.
 
 Question: {question}
 
-Helpful Answer: 
+Helpful Answer:
 """
 
 complete_prompt = ChatPromptTemplate.from_template(template=complete_prompt_template)
@@ -39,3 +43,28 @@ response = complete_prompt_chain.invoke(
     {"question": "Tell me a joke in 20 words or less."}
 )
 print("From Template: ", response.content)
+
+
+placeholder_prompt = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(
+            content="""
+    You're a helpful assistant that can answer the question about the fruit only.
+    If you don't know, just say 'I don\'t know' and don't try to make an answer.
+  """
+        ),
+        MessagesPlaceholder("messages"),
+    ]
+)
+
+placeholder_prompt_chain = placeholder_prompt | llm
+response = placeholder_prompt_chain.invoke(
+    {
+        "messages": [
+            HumanMessage(
+                content="How's the taste of grape, answer in 20 words or less."
+            )
+        ]
+    }
+)
+print("From Message Placeholder", response.content)
